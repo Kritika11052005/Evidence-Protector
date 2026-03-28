51f25e63ad655b6d3533f0cdc2a433d273771d2d
KRITIKA BENJWAL <ananya.benjwal@gmail.com>
2026-03-28 23:44:01 +0530

    changes for backend

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI(title="Evidence Protector API")
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001"],
+    allow_origins=["http://localhost:3000", "http://localhost:3001","https://evidence-protector.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
